1078913de032c81ba106dbf3fec574e83cfde80b
Tuan Anh <letuananh0301.dev@gmail.com>
2022-11-28 04:19:19 +0700

    fix env BE and add Admin

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -20,7 +20,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-app.use(express.static(path.join(__dirname, './public/assets')));
+app.use('/assets',express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
